8ab0dc7a043a9233f407ea935c396effaa08772d
accarneiro4 <amanda.caroline063@outlook.com>
2024-03-20 21:59:23 -0300

    Conteúdo da aula do dia 20-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/amandacaroline/secao04/ConversaoTipos02.java
@@ -0,0 +1,14 @@
+package br.com.senaisp.bauru.amandacaroline.secao04;
+
+public class ConversaoTipos02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte num1 = 127;
+		num1++;
+		num1++;
+		System.out.println(num1);
+		System.out.println((int)3.5);
+	}
+
+}

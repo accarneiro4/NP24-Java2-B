4f00f35a00564613b2907fd3561b63e0183b2ccd
accarneiro4 <amanda.caroline063@outlook.com>
2024-03-25 21:56:52 -0300

    conteúdo da aula do dia 25-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/amandacaroline/licao04/ExemploRandom.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.amandacaroline.licao04;
+
+import java.util.Random;
+
+public class ExemploRandom {
+	
+	public static void main (String[] args) {
+		Random rnd = new Random ();
+		System.out.println(rdn.nextInt (60)+1);
+		System.out.println(rdn.nextInt (60)+1);
+		System.out.println(rdn.nextInt (60)+1);
+		System.out.println(rdn.nextInt (60)+1);
+		System.out.println(rdn.nextInt (60)+1);
+	}
+
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/amandacaroline/licao04/exercicio/Exercicio01.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.amandacaroline.licao04.exercicio;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		MetodosCalculo mc = new MetodosCalculo ();
+		double tempC = mc.converterF2c(60.5);
+		System.out.println("Temperatura" + tempC);
+
+	}
+
+}
